0f75dff5c8b79f974549c2c2d5f7291152fc3a91
proninja123 <proninjacode07@gmail.com>
2023-01-16 18:24:48 +0530

    Deploy

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "8f1c1771ddbd23c168a9faf9df9c06b9",
 "favicon.ico": "1935503fb540eca75d601c7c96ed05de",
-"index.html": "acf970aa885018d39296b4870e70dc82",
-"/": "acf970aa885018d39296b4870e70dc82",
+"index.html": "6d047c94f67fa26c711a26be3d5bea30",
+"/": "6d047c94f67fa26c711a26be3d5bea30",
 "main.dart.js": "f805a199795b06ed9697be50aa046d88",
 "icons/favicon-16x16.png": "ff6712da61b0a536cc2689ac8058667f",
 "icons/favicon.ico": "1935503fb540eca75d601c7c96ed05de",
